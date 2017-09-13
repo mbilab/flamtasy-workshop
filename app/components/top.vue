73c78292a769676ad842000bdce976b46dfd61ee
goho302jo03 <goho302jo03@gmail.com>
2017-09-13 18:19:15 +0800

    fix(app/components/top): refine menu style

--- app/components/top.vue
+++ app/components/top.vue
@@ -1,13 +1,13 @@
 <template lang='pug'>
 .top
-  .image: img(id=logo src='../img/logo.png')
+  .image: img(v-on:click='panelChange(1)' id=logo src='../assets/img/logo.png')
   .menu
     li(v-on:click='panelChange(1)') 課程資訊
     li(v-on:click='panelChange(2)') 關於我們
-    li(v-on:click='panelChange(3)')  課程影片
-    li(v-on:click='panelChange(4)')  學員作品
-    li(v-on:click='panelChange(5)')  聯絡方式
-  .line: img(id=bar src='../img/line.png')
+    li(v-on:click='panelChange(3)') 課程影片
+    li(v-on:click='panelChange(4)') 學員作品
+    li(v-on:click='panelChange(5)') 聯絡方式
+  .line: img(id=bar src='../assets/img/line.png')
 </template>
 
 <script>
@@ -25,21 +25,26 @@ export default {
 img
   width: 100%
 li
-  font-family: Microsoft JhengHei
   font-weight: bold
-  font-size: 1.862vw
+  font-size: 1.6vw
+  border-radius: 5px
   display: inline
-  margin: 0
-  padding: 0 5.1042vw 0 0
+  padding: 0.262vw
+  margin: 0 5.1042vw 0 0
+  cursor: hand
+li:hover
+  background: #D1E6D7
+  color: #19826D
 .image
   padding: 0 2.917vw 0 1.5625vw
   width: 11.302vw
   float: left
+  cursor: hand
 .line
   clear: left
   height: 1.0417vw
 .menu
-  padding-top: 9.0104vw
+  padding: 9.0104vw 0 1.0417vw
 .top
   padding: 0.9375vw 0 1.5625vw 0
 #bar
